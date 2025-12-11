7da7ddfdd23e346bf92801c42b1eb7efde351c85
petreleven <eng.peter.ndungu@gmail.com>
2025-12-11 17:54:20 +0300

    1st

---
+++ main.py
@@ -0,0 +1,22 @@
+import flask
+
+app = flask.Flask("task app")
+
+tasks = []
+
+
+@app.route("/view_tasks", methods=["get", "post"])
+def view_tasks():
+    if flask.request.method == "POST":
+        task = flask.request.form.get("task_name")
+        tasks.remove(task)
+    return flask.render_template("view_tasks.html", tasks=tasks)
+
+
+@app.route("/add_tasks", methods=["get", "post"])
+def add_tasks():
+    if flask.request.method == "POST":
+        task = flask.request.form.get("task")
+        print(task)
+        tasks.append(task)
+    return flask.render_template("add_tasks.html", tasks=tasks)
